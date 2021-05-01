ca4fe4109b7972832190eefd36c8535b3c151d39
Chris Worman <chrisworman@gmail.com>
2021-04-30 20:28:30 -0700

    Remove commented out Array inheritence

--- src/examples/CNF/CNFChromosome.ts
+++ src/examples/CNF/CNFChromosome.ts
@@ -1,6 +1,4 @@
-import ArrayChromosome from "../../helpers/arrayChromosome";
 import BooleanChromosome from "../../helpers/booleanChromosome";
 
-// export default class CNFChromosome extends ArrayChromosome<boolean> {
 export default class CNFChromosome extends BooleanChromosome {
 }
